e313b417b08d916902c09d15a8be9927ae86082c
Vadim Karasik <vadkarasik@gmail.com>
2019-02-12 16:57:43 +0300

    add verge

---
+++ app/verge/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Cache-control" content="no-cache">
+		<meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="Expires" content="-1">
+
+        <meta name="description" content="Часы поддерживают множество полезных для спорта функций. Они отлично подходят для занятий бегом, ходьбой, езды
+        на велосипеде, плавания, эллиптической машины, альпинизма, триатлона, тенниса, футбола и лыж.">
+        <title>Amazfit | Купить Amazfit Verge</title>
+        <?php include '../includes/head.html' ?>
+    </head>
+    <body>
+        <div class="container-fluid">
+        <?php 
+            include '../includes/header.html';
+            include '../includes/banner-verge.html';
+            include '../includes/shops.html';
+            include '../includes/footer.html';
+        ?>
+        </div>
+        <?php  include '../includes/count.html' ?>
+    </body>
+</html>
